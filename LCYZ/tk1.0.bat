4759be7cdf8b27e6888b8e855e845a4cb19c824a
Chen-Jin <158279240+Chen-Jin-1@users.noreply.github.com>
2025-02-26 14:44:05 +0800

    Add files via upload

--- LCYZ/tk1.0.bat
+++ LCYZ/tk1.0.bat
@@ -8,7 +8,7 @@ taskkill /f /im LBD_TeachClientEx.exe
 taskkill /f /im LgClientWebSocketServer.exe
 taskkill /f /im LgClientInstallTool.exe
 taskkill /f /im LgClientUpdateService.exe
-tasskill /f /im LBD_ClientUDPCH.exe
+taskkill /f /im LBD_ClientUDPCH.exe
 rd /s /q LBD5800MT
 rd /s /q LBDCMD_Tools
 rd /s /q LgSoftWAN
