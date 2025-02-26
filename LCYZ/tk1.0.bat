c12d0adc3e696b541848f1c644580f995f299e29
Chen-Jin <158279240+Chen-Jin-1@users.noreply.github.com>
2025-02-26 14:05:56 +0800

    Create tk1.0.bat

---
+++ LCYZ/tk1.0.bat
@@ -0,0 +1,17 @@
+:: 脱控 1.0
+:: 制作 Chen-Jin
+:: 已在临川一中高新校区初中部电脑室测试成功
+
+cd /d "C:\Program Files (x86)\"
+taskkill /f /im MMClient.exe
+taskkill /f /im CDMPCom.exe
+taskkill /f /im CDMPToolsService.exe
+taskkill /f /im wincontrol_vdi.exe
+taskkill /f /im LBD_TeachClientEx.exe
+taskkill /f /im LgClientWebSocketServer.exe
+taskkill /f /im LgClientInstallTool.exe
+taskkill /f /im LgClientUpdateService.exe
+rd /s /q LBD5800MT
+rd /s /q LBDCMD_Tools
+rd /s /q LgSoftWAN
+pause
